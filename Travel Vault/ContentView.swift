a22074a84b14e6c34aed034fb49fc24a583dbd10
Rahim Bardai <rahimbardai03@gmail.com>
2025-03-30 21:22:58 -0500

    Init

---
+++ Travel Vault/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Travel Vault
+//
+//  Created by Rahim Bardai on 3/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Rahim!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
